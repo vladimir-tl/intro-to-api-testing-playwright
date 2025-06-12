54d6523b3893004a53af63ef7c8b9a93418f429a
vladimirovodenko <vladimir@tallinn-learning.ee>
2025-06-12 19:53:06 +0300

    fixed prettier issues

--- tests/dto/order-dto.ts
+++ tests/dto/order-dto.ts
@@ -1,15 +1,20 @@
 export class OrderDto {
-
   // define fields of the class
-  status: string;
-  courierId: number;
-  customerName: string;
-  customerPhone: string;
-  comment: string;
+  status: string
+  courierId: number
+  customerName: string
+  customerPhone: string
+  comment: string
   id: number
 
   // create a constructor Ctrl + N or Cmd + N (Mac)
-  constructor(status: string, courierId: number, customerName: string, customerPhone: string, comment: string) {
+  constructor(
+    status: string,
+    courierId: number,
+    customerName: string,
+    customerPhone: string,
+    comment: string,
+  ) {
     this.status = status
     this.courierId = courierId
     this.customerName = customerName
@@ -17,5 +22,4 @@ export class OrderDto {
     this.comment = comment
     this.id = 0
   }
-
 }
